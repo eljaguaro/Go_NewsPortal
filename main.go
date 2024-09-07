b774d7b38ad2e3eaefb0881900d65d1b23035c36
Давид Заде <Davidzade21@mail.ru>
2024-09-07 18:35:14 +0300

    added the show_post handler, func and template

--- main.go
+++ main.go
@@ -7,15 +7,45 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	_ "github.com/lib/pq"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("postgres", fmt.Sprintf("host='localhost' port=5432 user='postgres2' password='David2004' dbname='ddd' sslmode=disable"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM articles")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	posts = []Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		posts = append(posts, post)
+	}
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -30,35 +60,65 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
-	connStr := "host='localhost' port=5432 user='postgres2' password='David2004' dbname='ddd' sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+
+		db, err := sql.Open("postgres", fmt.Sprintf("host='localhost' port=5432 user='postgres2' password='David2004' dbname='ddd' sslmode=disable"))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer db.Close()
+
+		insert, err := db.Query("INSERT INTO articles (title, anons, full_text) VALUES($1, $2, $3)", title, anons, full_text)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer insert.Close()
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func show_post(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("postgres", fmt.Sprintf("host='localhost' port=5432 user='postgres2' password='David2004' dbname='ddd' sslmode=disable"))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO articles (title, anons, full_text) VALUES($1, $2, $3)", title, anons, full_text)
+	res, err := db.Query("SELECT * FROM articles WHERE id = 1")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	// insert = insert
-	defer insert.Close()
-	// fmt.Println(insert.RowsAffected())
-	// fmt.Println(insert.RowsAffected())
-	// sel, err := db.Query(fmt.Sprintf("SELECT title FROM articles"))
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-	// defer insert.Close()
-	// fmt.Println(sel)
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	showPost = Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		showPost = post
+	}
+	t.ExecuteTemplate(w, "show", showPost)
 }
 
 func HandleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8085", nil)
 }
 
